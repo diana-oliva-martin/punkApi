39007e166ebaf594a75c17380ebf90ecdb7bbbfb
Diana Oliva <diana.oliva.martin@gmail.com>
2021-03-11 11:00:48 +0100

    Index correcto en public

--- public/index.php
+++ public/index.php
@@ -15,11 +15,14 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-/*$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+/*
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
-$kernel->terminate($request, $response);*/
+$kernel->terminate($request, $response);
+*/
+
 
 /* MAIN */
 
