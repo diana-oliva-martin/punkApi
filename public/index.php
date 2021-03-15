c593a93df6f75ac5a75dae78dd3af95c6b096457
Diana Oliva <diana.oliva.martin@gmail.com>
2021-03-15 10:49:56 +0100

    Prueba controlador con plantilla twig

--- public/index.php
+++ public/index.php
@@ -15,48 +15,8 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-/*
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
-*/
-
-
-/* MAIN */
-
-# Prueba Petición a la API PunkApi desde el cliente HTTP Guzzle
-$client = new GuzzleHttp\Client();
-
-// Petición Búsqueda
-$url = "https://api.punkapi.com/v2/beers?food=food";
-//$url = "https://api.punkapi.com/v2/beers?beer_name=food";
-$res = $client->request('GET', $url);
-//echo "Code Response: ".$res->getStatusCode(); //200
-//echo "<br>";
-
-//echo "Output format: ".$res->getHeader('content-type')[0]; // 'application/json; charset=utf8'
-//echo "<br>";
-
-//echo "Response: ";
-//echo "<br>";
-$dataBeers = $res->getBody();
-
-$beers = json_decode($dataBeers, true);
-
-// Petición listado
-//
-// 1. Primera petición: Devolver id, name, description
-foreach ($beers as $beer) {
-    echo $beer['id'].', '.$beer['name'].', '.$beer['description'];
-    echo "<br>";
-    echo "<br>";
-}
-
-// 2. Segunda petición: Devolver id, name, description, imagen, slogan (tagline) y first_brewed
-foreach ($beers as $beer) {
-    echo $beer['id'].', '.$beer['name'].', '.$beer['description'].', '.$beer['image_url'].', '.$beer['tagline'].', '.$beer['first_brewed'];
-    echo "<br>";
-    echo "<br>";
-}
--- src/Controller/BeersController.php
+++ src/Controller/BeersController.php
@@ -19,14 +19,52 @@ class BeersController extends AbstractController
         ]);*/
         $food = random_int(0, 100);
 
-        return new Response(
-          '<html><body>Lucky number:' .$food.'</body></html>'
-        );
+        /*return new Response(
+            '<html><body>Lucky number:' . $food . '</body></html>'
+        );*/
         return $this->render('beers/food.html.twig', [
-            'food' => $food,
+            'food' => $food
         ]);
     }
+}
+/*
+# Prueba Petición a la API PunkApi desde el cliente HTTP Guzzle
+        $client = new GuzzleHttp\Client();
 
+// Petición Búsqueda
+        $url = "https://api.punkapi.com/v2/beers?food=food";
+//$url = "https://api.punkapi.com/v2/beers?beer_name=food";
+        $res = $client->request('GET', $url);
+//echo "Code Response: ".$res->getStatusCode(); //200
+//echo "<br>";
+
+//echo "Output format: ".$res->getHeader('content-type')[0]; // 'application/json; charset=utf8'
+//echo "<br>";
+
+//echo "Response: ";
+//echo "<br>";
+        $dataBeers = $res->getBody();
+
+        $beers = json_decode($dataBeers, true);
+
+// Petición listado
+//
+// 1. Primera petición: Devolver id, name, description
+        foreach ($beers as $beer) {
+            echo $beer['id'].', '.$beer['name'].', '.$beer['description'];
+            echo "<br>";
+            echo "<br>";
+        }
+
+// 2. Segunda petición: Devolver id, name, description, imagen, slogan (tagline) y first_brewed
+        foreach ($beers as $beer) {
+            echo $beer['id'].', '.$beer['name'].', '.$beer['description'].', '.$beer['image_url'].', '.$beer['tagline'].', '.$beer['first_brewed'];
+            echo "<br>";
+            echo "<br>";
+        }
+
+    }
+*/
    /* public function food()
     {
         $food = random_int(0, 100);
@@ -35,4 +73,5 @@ class BeersController extends AbstractController
             'food' => $food,
         ]);
     }*/
-}
+//}
+//}
