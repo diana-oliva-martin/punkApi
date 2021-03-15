e9a975d3c01d4495c17de7a4630382114c2d8042
Diana Oliva <diana.oliva.martin@gmail.com>
2021-03-15 13:17:51 +0100

    Mejoras

--- src/Beers.php
+++ src/Beers.php
@@ -10,15 +10,21 @@ class Beers
 {
     const URL_API = 'https://api.punkapi.com/v2/beers?food=food';
 
-    public static function request() {
+    public static function request()
+    {
 
-        //Petición a la API PunkApi desde el cliente HTTP Guzzle
+        //Request to the PunkApi API from the HTTP Guzzle client
         $clientGuzzle = new Client();
 
-        $res = $clientGuzzle->request('GET', Beers::URL_API);
-        $dataBeers = $res->getBody();
-
-        return json_decode($dataBeers, true);
+        try {
+            $res = $clientGuzzle->request('GET', Beers::URL_API);
+            $dataBeers = $res->getBody();
+            return json_decode($dataBeers, true);
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+            echo 'An error occurred while trying to get the beers by punkAPI. ' . $e->getMessage();
+        } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+            echo 'An error occurred while trying to get the beers by punkAPI. ' . $e->getMessage();
+        }
     }
 
 }
